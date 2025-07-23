7060146352ef00a5d9cb29e95334d8e2ec0565d6
rayneke <pytisem@gmail.com>
2025-07-23 14:52:41 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById('view-progress').onclick = () => {
+  alert('Откроется страница с графиками');
+};
+
+document.getElementById('add-training').onclick = () => {
+  alert('Откроется форма добавления тренировки');
+};
+
+document.getElementById('delete-training').onclick = () => {
+  alert('Откроется список для удаления тренировки');
+};
